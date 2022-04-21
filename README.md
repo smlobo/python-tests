e10c6aa1433cf6a5b09243a143fe297ee03d3582
Sheldon Lobo <sheldon.lobo@pendo.io>
2022-04-21 19:42:30 -0400

    Added the `requests-api-json` example

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # python-tests
 
 ## json-file
+* encode data to json and write to file
+* read from file and decode json back to python data structures
 ```
 % cd json-file
 % ./data-to-file.py 
@@ -15,3 +17,19 @@ Read in dictionary value for "k1" :  1
 Read in dictionary value for "k2" :  v2
 Read in dictionary value for "k3" :  3.33
 ```
+
+## requests-api-json
+* use the requests package to call an api
+* decode the results using the inbuilt json package
+```
+% cd requests-api-json
+% /usr/local/bin/pip3 install requests
+% ./query-print-fred.py 
+US Population:
+Date		Population in millions
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+2021-06-01	332043.302
+2021-07-01	332140.523
+...
+```
+
---
+++ requests-api-json/query-print-fred.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+import json
+import requests
+
+# Query an API, decode the JSON returned.
+
+# My API key (in a URL parameter)
+API_KEY = "&api_key=e12c0787f51ff6db24ac8029710fa175"
+
+# FRED API query URL asking for US population data
+FRED_API = "https://api.stlouisfed.org/fred/series/observations?series_id=POP"
+
+# Returned data format
+FORMAT = "&file_type=json"
+
+# Time range for data
+RANGE = "&observation_start=2021-06-01&observation_end=2022-01-01"
+
+# Complete URL
+url = FRED_API + FORMAT + API_KEY + RANGE
+
+# Use the "requests" package to make the query
+response = requests.get(url)
+
+# Print raw data (make pretty by cut-n-paste to https://jsonformatter.org/json-pretty-print)
+# (see `raw-json-pretty.txt`)
+# print("Raw output -> {}".format(response.text))
+
+# Decode the data using the python "json" package
+decoded_data = json.loads(response.text)
+
+# Print decoded types
+# print(type(decoded_data))
+# print(type(decoded_data["observations"]))
+
+# Print some decoded data
+print("US Population:")
+print("Date\t\tPopulation in millions")
+print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+for data_item in decoded_data["observations"]:
+    print("{}\t{}".format(data_item["date"], data_item["value"]))
